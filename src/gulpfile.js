92637eaa45fe2a04ee3be3ec1c6b2b6292b7d6af
cfr <1771952134@qq.com>
2018-08-06 10:23:50 +0800

    33

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var server = require('gulp-webserver');
 var path = require('path');
 var fs = require('fs');
-var data = require('./mock/data/data.json')
+var data = require('./mock/data/data.json');
+var minJs = require('gulp-uglify');
+// var minCss = require('gulp-min-css');
 gulp.task('server', function () {
     gulp.src('mock')
         .pipe(server({
@@ -21,4 +23,22 @@ gulp.task('server', function () {
                 }
             }
         }))
+})
+//压缩
+gulp.task('js', function () {
+    gulp.src('mock/js/flexible.js')
+        .pipe(minJs())
+        .pipe(gulp.dest('mock/min'))
+})
+// gulp.task('css', function () {
+//     gulp.src('mock/sass/style.css')
+//         .pipe(minCss())
+//         .pipe(gulp.dest('mock/min'))
+// })
+// watch 自动刷新
+gulp.task('watchs', function () {
+    gulp.watch('mock/sass/style.scss', ['server'])
+})
+gulp.task('default', function () {
+    gulp.src('mock', ['server', 'js'])
 })
